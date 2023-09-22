8a4cf55254bfac5d01dcead729f2b1e2a396c7ad
Maximka01 <mlokotkov@ya.ru>
2023-09-22 21:43:32 +0300

    bazasave

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-data = pd.read_csv("train/train_meta.csv")
-with pd.option_context("display.max_rows", None, "display.max_columns", None):
-    print(data.head(5))
 
 
+def f2():
+    data1 = pd.read_csv("task_1/train/train_meta.csv")
+    data2 = pd.read_csv("task_1/train/train_gts.csv")
+    pd.set_option('display.max_columns', None)
+    # print(data1)
+    # print(list(data1.columns.values))
+    for i in range(1, 10):
+        print(data1.loc[[i]])
+    # for i in range(1, 100):
+    #     print(data2.loc[[i]])
 
-# with open("test/01165_hr.npy", "rb") as f:
-#      data1 = np.load(f, allow_pickle=True)
-#
-# with open("test/10716_hr.npy", "rb") as f:
-#     data2 = np.load(f, allow_pickle=True)
+
+def f1():
+    with open("task_1/train/00617_hr.npy", "rb") as f:
+        data3 = np.load(f, allow_pickle=True)
+    with open("task_1/train/00034_hr.npy", "rb") as f:
+        data4 = np.load(f, allow_pickle=True)
+
+    for i in range(len(data3)):
+        plt.plot(data4[i], color='r')
+        plt.plot(data3[i], color='b')
+        plt.show()
+
+
+# f2()
+f1()
