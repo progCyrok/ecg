50564379f998e093abe88c3f231287dda72e770a
Gosha <goshavlasov2005@gmail.com>
2023-09-23 20:30:22 +0300

    added normdata

--- task_1/funktions.py
+++ task_1/funktions.py
@@ -1,6 +1,8 @@
 class Funk():
     def __init__(self):
-        self.stadium = {'unknown', "nan", 'Stadium II', 'Stadium III', 'Stadium I-II', 'Stadium I', 'Stadium II-III'}
+        self.male_female_stat = 3.46
+        self.heart_axis= {"NaN", 'ALAD', 'AXR', 'RAD', 'MID', 'SAG', 'ARAD', 'AXL', 'LAD'}
+        self.stadium = {'Stadium II', 'Stadium III', 'Stadium I-II', 'Stadium I', 'Stadium II-III'}
     def infarction_stadium(self,stadium):
         if stadium == "unknown" or stadium == "nan":
             return 0
@@ -13,3 +15,33 @@ class Funk():
         elif stadium == 'Stadium II-III':
             return 0.8
         return 1
+    def sex(self,sex):
+        if int(sex) == 1:
+            return 1
+        return 1/self.male_female_stat
+    def age(self,age):
+        return age/100
+    def imt(self,weight,height):
+        n_height = height/100
+        imt = weight/n_height**2
+        if imt <= 18:
+            return 0.25
+        elif 18 < imt <= 24:
+            return 0.5
+        if 24 < imt <= 30:
+            return 0.75
+        return 1
+    def dependence_stadium(self,stad1,stad2):
+        if stad1 in self.stadium and stad2 in self.stadium:
+            return 1
+        return 0
+    def validated_by_human(self,validated_by_human):
+        if validated_by_human == True:
+            return 1
+        return 0
+    def pacemaker(self,pacemaker):
+        if pacemaker != 'nan':
+            return 0
+        return 1
+
+
--- task_1/main.py
+++ task_1/main.py
@@ -5,23 +5,50 @@ import matplotlib.pyplot as plt
 
 def f2():
     data1 = pd.read_csv("train/train_meta.csv")
-    sr = set()
-    for data in data1["infarction_stadium1"]:
-        sr.add(data)
-    print(sr)
+    data2 = pd.read_csv("train/train_gts.csv")
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.expand_frame_repr', False)
+    sr1 = set()
+    sr2 = set()
+    sr3 = set()
+    sr4 = set()
 
+    name = list(data1['record_name'])
+    meta = [-1] * len(name)
+    for i in range(len(name)):
+        df = data2.loc[data2['record_name'] == name[i]]
+        meta[i] = df['myocard'].loc[df.index[0]]
+    data1['myocard'] = meta
+    print(data1[['pacemaker','extra_beats']])
 
-def f1():
-    with open("train/00034_hr.npy", "rb") as f:
-        data3 = np.load(f, allow_pickle=True)
-    with open("train/00034_hr.npy", "rb") as f:
-        data4 = np.load(f, allow_pickle=True)
+def pandas_add():
+    data1 = pd.read_csv("train/train_meta.csv")
+    data2 = pd.read_csv("train/train_gts.csv")
+    name = list(data1['record_name'])
+    meta = [-1] * len(name)
+    for i in range(len(name)):
+        df = data2.loc[data2['record_name'] == name[i]]
+        meta[i] = df['myocard'].loc[df.index[0]]
+    data1['myocard'] = meta
+    dt = {'record_name':[],'stadium1':[],'stadium2':[],'sex':[],'age':[],'imt':[],'dependence_stadium':[],'validated_by_human':[],
+          'pacemaker':[],'myocard':[]}
+    dataframe_pandas = pd.DataFrame(dt)
+    import funktions
+    funk = funktions.Funk()
+    for i in range(len(data1['record_name'])):
+        arr = []
+        arr.append(data1['record_name'][i])
+        arr.append(funk.infarction_stadium(data1['infarction_stadium1'][i]))
+        arr.append(funk.infarction_stadium(data1['infarction_stadium2'][i]))
+        arr.append(funk.sex(data1['sex'][i]))
+        arr.append(funk.age(data1['age'][i]))
+        arr.append(funk.imt(data1['weight'][i],data1['height'][i]))
+        arr.append(funk.dependence_stadium(data1['infarction_stadium1'][i],data1['infarction_stadium2'][i]))
+        arr.append(funk.validated_by_human(data1['validated_by_human'][i]))
+        arr.append(funk.pacemaker(data1['pacemaker'][i]))
+        arr.append(data1['myocard'][i])
+        dataframe_pandas.loc[len(dataframe_pandas.index)] = arr
+    dataframe_pandas.to_csv('file_without_anilise_of_graphics.csv')
 
-    for i in range(len(data3)):
-        plt.plot(data4[i], color='r')
-        plt.plot(data3[i], color='b')
-        plt.show()
-
-
-f2()
-# f1()
+pandas_add()
